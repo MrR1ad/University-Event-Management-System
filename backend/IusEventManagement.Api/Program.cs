2cf006923d7f7d68318ab135d6c09fd31a312964
MrR1ad <riad.elezovic@outlook.com>
2026-04-06 18:27:40 +0200

    AddDbContext added

--- backend/IusEventManagement.Api/Program.cs
+++ backend/IusEventManagement.Api/Program.cs
@@ -1,7 +1,13 @@
+using IusEventManagement.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
